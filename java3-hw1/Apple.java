81cf60df9311c5e2751d3cc21a3526d9de492040
mpolyakov <polyakov4@yandex.ru>
2019-06-11 00:10:59 +0300

    deleted classes

--- java3-hw1/Apple.java
+++
@@ -1,7 +0,0 @@
-public class Apple extends Fruit {
-    public final int WEIGHT = 1;
-
-    public int getWEIGHT() {
-        return WEIGHT;
-    }
-}
--- java3-hw1/Orange.java
+++
@@ -1,7 +0,0 @@
-public class Orange extends Fruit {
-    public final int WEIGHT = 2;
-
-    public int getWEIGHT() {
-        return WEIGHT;
-    }
-}
