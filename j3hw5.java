f603ac4ce59c5a837c7e0ed0706bc2dc58a3ae35
mpolyakov <polyakov4@yandex.ru>
2019-06-26 00:14:45 +0300

    java3 hw 5

---
+++ j3hw5.java
@@ -0,0 +1,161 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch; // for start/finish
+import java.util.concurrent.Semaphore;      // for tunnel
+
+public class j3hw5 {
+    public static final int CARS_COUNT = 4;
+    public static final CountDownLatch START = new CountDownLatch(CARS_COUNT + 1);
+    public static final Semaphore TUNNEL = new Semaphore(CARS_COUNT / 2, false);
+    public static final CountDownLatch FINISH = new CountDownLatch(CARS_COUNT);
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(70), new Tunnel(), new Road(50));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int)(Math.random() * 10));
+            new Thread(cars[i]).start();
+        }
+
+        while (START.getCount() > 1)
+            Thread.sleep(100);
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        START.countDown();
+
+        while (FINISH.getCount() > 0);
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
+
+class Car implements Runnable {
+    private static int CARS_COUNT;
+    static {
+        CARS_COUNT = 0;
+    }
+    private Race race;
+    private int speed;
+    private String name;
+
+    public Car(Race race, int speed) {
+        this.race = race;
+        this.speed = speed;
+        this.name = "Участник #" + (++CARS_COUNT);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(name + " готовится");
+            Thread.sleep(500 + (int)(Math.random() * 800));
+            System.out.println(name + " готов (" + speed + ")");
+
+            j3hw5.START.countDown();
+            j3hw5.START.await();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+
+        if (j3hw5.FINISH.getCount() ==
+                j3hw5.CARS_COUNT)
+            System.out.println(name + " - победил");
+
+        try {
+            j3hw5.FINISH.countDown();
+            j3hw5.FINISH.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+abstract class Stage {
+    protected int length;
+    protected String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract void go(Car car);
+}
+
+class Road extends Stage {
+
+    public Road(int length) {
+        this.length = length;
+        this.description = "Дорога " + length + " метров";
+    }
+
+    @Override
+    public void go(Car car) {
+        try {
+            System.out.println(
+                    car.getName() + " начал этап: " + description);
+            Thread.sleep(length / car.getSpeed() * 1000);
+            System.out.println(
+                    car.getName() + " закончил этап: " + description);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Tunnel extends Stage {
+
+    public Tunnel() {
+        this.length = 180;
+        this.description = "Тоннель " + length + " метров";
+    }
+
+    @Override
+    public void go(Car car) {
+        try {
+            try {
+                System.out.println(car.getName() +
+                        " готовится к этапу (ждёт): " + description);
+
+                j3hw5.TUNNEL.acquire();
+
+                System.out.println(car.getName() +
+                        " начал этап: " + description);
+                Thread.sleep(length / car.getSpeed() * 1000);
+
+                j3hw5.TUNNEL.release();
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(car.getName() +
+                        " закончил этап: " + description);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Race {
+    private ArrayList<Stage> stages;
+
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+
+    public ArrayList<Stage> getStages() {
+        return stages;
+    }
+}
